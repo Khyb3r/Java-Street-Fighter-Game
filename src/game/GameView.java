2163af9d99d37b448b04cfdd01ad04cf03af2905
Khyb3r <Khyber.Jan@city.ac.uk>
2024-02-19 00:25:57 +0000

    Inheritance

--- src/game/GameView.java
+++ src/game/GameView.java
@@ -1,9 +1,20 @@
 package game;
 import city.cs.engine.UserView;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GameView extends UserView {
+    private Image background;
+
     public GameView(GameWorld world, int width, int height) {
         super(world, width, height);
+        this.background = new ImageIcon("data/books.png").getImage();
+    }
+
+    @Override
+    protected void paintBackground(Graphics2D g) {
+        g.drawImage(background,0,0,this);
     }
 }
 
